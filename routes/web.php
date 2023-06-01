79da9acd3f775548fa9b2676ccc9a4a5cb27599c
Guillermo Enrique <gsanchez1687@gmail.com>
2023-06-01 08:28:41 -0500

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 //AUTHOR INDEX
-Route::get('/authors','AuthorController@index');
-//Route::get('/authors', [App\Http\Controllers\AuthorController::class, 'index'])->name('index');
+Route::get('/api/v1/authors','AuthorController@index');
 
 //AUTHOR STORE
-Route::post('/authors','AuthorController@store');
+Route::post('/api/v1/authors','AuthorController@store');
 
 //AUTHOR SHOW
-Route::get('/authors/{author}','AuthorController@show');
+Route::get('/api/v1/authors/{author}','AuthorController@show');
 
 //AUTHOR UPDATE
-Route::put('/authors/{author}','AuthorController@update');
+Route::put('/api/v1/authors/{author}','AuthorController@update');
 
 //AUTHOR DESTROY
-Route::patch('/authors/{author}','AuthorController@destroy');
+Route::patch('/api/v1/authors/{author}','AuthorController@destroy');
